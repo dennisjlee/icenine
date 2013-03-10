b0d7184a27de518fb35e22c5b26258f34d7d12a6
DJ Lee <dennisjlee@gmail.com>
2013-03-09 21:44:21 -0500

    Finish api endpoints for tv/movie dirs and files

--- icenine2/application.py
+++ icenine2/application.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from flask import Flask, render_template, request, url_for
+from flask import Flask, abort, render_template, request, url_for
 from flask.ext.pymongo import PyMongo
 from bson import json_util
 import json
@@ -20,27 +20,27 @@ def mongo_jsonify(doc):
       mimetype='application/json')
 
 
-@app.route("/api/movies/")
-@app.route("/api/movies/<path:path>")
-def movies(path=None):
-  if path is not None and path[-1] != '/':
-    f = mongo.db.files.find_one({'type': 'movie', 'relative_path': path})
+@app.route("/api/<filetype>/", defaults={'path': '/'})
+@app.route("/api/<filetype>/<path:path>")
+def movies(filetype, path):
+  if filetype != 'movie' and filetype != 'tv':
+    abort(404)
+
+  if path[-1] != '/':
+    f = mongo.db.files.find_one({'type': filetype, 'relative_path': path})
     return mongo_jsonify(f)
   else:
-    if path is None:
-      path = ''
-    else:
-      path = path[:-1]
-    d = mongo.db.directories.find_one({'type': 'movie', 'relative_path':
+    path = path[:-1]
+    d = mongo.db.directories.find_one({'type': filetype, 'relative_path':
         path, 'found': True})
-    # TODO: load and serialize child directories and child files
+    exclude_fields = {'_id': False, 'type': False, 'parent_id': False, 'found': False}
+    d['subdirectories'] = list(mongo.db.directories.find(
+        {'type': filetype, 'parent_id': d['_id'], 'found': True},
+        exclude_fields))
+    d['files'] = list(mongo.db.files.find(
+        {'type': filetype, 'parent_id': d['_id'], 'found': True},
+        exclude_fields))
     return mongo_jsonify(d)
-  return "Movie requested, path = '%s'" % path
-
-@app.route("/api/tv/")
-@app.route("/api/tv/<path:path>")
-def tv(path=None):
-  return "TV requested, path = '%s'" % path
 
 if __name__ == "__main__":
   app.run(debug=True)
