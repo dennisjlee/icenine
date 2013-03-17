afcafbf623d5a2ef15dca9d7d45efb99f15dc037
DJ Lee <dennisjlee@gmail.com>
2013-03-16 21:32:27 -0400

    Initial stab at using Backbone

--- icenine2/application.py
+++ icenine2/application.py
@@ -11,23 +11,32 @@ app.config.from_envvar('ICENINE_SETTINGS')
 
 mongo = PyMongo(app)
 
-@app.route("/", defaults={'path': None})
-@app.route("/tv/<path:path>")
-@app.route("/movies/<path:path>")
+@app.route('/', defaults={'path': None})
 def index(path):
   return render_template('index.html',
       static_prefix=url_for('static', filename=''),
       use_cdn=app.config['USE_CDN'])
+
+# Note: tv and movies need separate routes to avoid redirecting from /tv/ to
+# /movies/
+@app.route('/tv/', defaults={'path': '/'})
+@app.route('/tv/<path:path>')
+def tv(path):
+  return index(path)
+
+@app.route('/movies/', defaults={'path': '/'})
+@app.route('/movies/<path:path>')
+def movies(path):
+  return index(path)
 
 def mongo_jsonify(doc):
   return app.response_class(
       json.dumps(doc, default=json_util.default, indent=None if request.is_xhr else 2),
       mimetype='application/json')
 
-
-@app.route("/api/<filetype>/", defaults={'path': '/'})
-@app.route("/api/<filetype>/<path:path>")
-def movies(filetype, path):
+@app.route('/api/metadata/<filetype>/', defaults={'path': '/'})
+@app.route('/api/metadata/<filetype>/<path:path>')
+def metadata_api(filetype, path):
   if filetype != 'movie' and filetype != 'tv':
     abort(404)
 
@@ -38,14 +47,24 @@ def movies(filetype, path):
     path = path[:-1]
     d = mongo.db.directories.find_one({'type': filetype, 'relative_path':
         path, 'found': True})
-    exclude_fields = {'_id': False, 'type': False, 'parent_id': False, 'found': False}
-    d['subdirectories'] = list(mongo.db.directories.find(
-        {'type': filetype, 'parent_id': d['_id'], 'found': True},
-        exclude_fields))
-    d['files'] = list(mongo.db.files.find(
-        {'type': filetype, 'parent_id': d['_id'], 'found': True},
-        exclude_fields))
     return mongo_jsonify(d)
 
-if __name__ == "__main__":
+EXCLUDE_FIELDS = {'type': False, 'parent_id': False, 'found': False}
+
+# TODO: id parsing below may need to change when using Mongo-generated object
+# ids instead of mysql-generated numeric ids
+
+@app.route('/api/subdirs/<int:parent_id>')
+def subdirs_api(parent_id):
+  return mongo_jsonify(list(mongo.db.directories.find(
+      {'parent_id': parent_id, 'found': True},
+      EXCLUDE_FIELDS)))
+
+@app.route('/api/files/<int:parent_id>')
+def files_api(parent_id):
+  return mongo_jsonify(list(mongo.db.files.find(
+      {'parent_id': parent_id, 'found': True},
+      EXCLUDE_FIELDS)))
+
+if __name__ == '__main__':
   app.run(debug=True)
--- icenine2/static/js/icenine.js
+++ icenine2/static/js/icenine.js
@@ -1,3 +1,92 @@
+var curDir = null;
+
+var PATH_TO_TYPES = {
+  'movies': 'movie',
+  'tv': 'tv'
+};
+
+var File = Backbone.Model.extend({
+  idAttribute: "_id",
+  url: function() {
+    return '/api/metadata/' + this.fullPath();
+  },
+  fullPath: function() {
+    return this.get('type') + '/' + this.get('relative_path');
+  },
+  additionDate: function() {
+    if (this.has('addition_date')) {
+      return new Date(this.get('addition_date')['$date']);
+    }
+    return null;
+  }
+});
+
+var Directory = Backbone.Model.extend({
+  idAttribute: "_id",
+  url: function() {
+    return '/api/metadata/' + this.fullPath();
+  },
+  fullPath: function() {
+    return this.get('type') + '/' + this.get('relative_path') +
+        ((this.get('relative_path') != '') ? '/' : '');
+  },
+  initialize: function() {
+    this.subdirs = new Directories([], {parent: this});
+
+    this.files = new Files([], {parent: this});
+  }
+});
+
+var Directories = Backbone.Collection.extend({
+  model: Directory,
+  url: function() {
+    return '/api/subdirs/' + this.parent.id;
+  },
+  initialize: function(models, options) {
+    this.parent = options.parent;
+  }
+});
+
+
+var Files = Backbone.Collection.extend({
+  model: File,
+  url: function() {
+    return '/api/files/' + this.parent.id;
+  },
+  initialize: function(models, options) {
+    this.parent = options.parent;
+  }
+});
+
+var IndexView = Backbone.View.extend({
+  // TODO: use a template and spruce this up
+  render: function() {
+    this.$el.html(
+        '<a data-fragment="movies/" href="movies/">Movies</a>' +
+        '<br>' +
+        '<a data-fragment="tv/" href="tv/">TV</a>');
+    return this;
+  },
+
+  events: {
+    'click a': function(e) {
+      e.preventDefault();
+      Backbone.history.navigate(e.target.getAttribute('data-fragment'),
+          {'trigger': true});
+    }
+  }
+});
+
+var DirectoryView = Backbone.View.extend({
+  initialize: function() {
+    this.listenTo(this.model, "change", this.render);
+  },
+
+  render: function() {
+    this.$el.html('This is directory ' + this.model.fullPath());
+  }
+});
+
 var IcenineRouter = Backbone.Router.extend({
   routes: {
     '':             'index',
@@ -6,11 +95,34 @@ var IcenineRouter = Backbone.Router.extend({
   },
 
   index: function() {
-    console.log('navigated to index');
+    var indexView = new IndexView();
+    indexView.render();
+    $('#content').append(indexView.el);
   },
 
   tv: function(path) {
-    console.log('navigated to tv with path ' + path);
+    if (path == '') path = '/';
+    if (path[path.length - 1] == '/') {
+      if (!(curDir &&
+          curDir.get('type') == 'tv' &&
+          curDir.get('relative_path') == path)) {
+        curDir = new Directory({
+          'type': 'tv',
+          'relative_path': path.substr(0, path.length - 1)
+        });
+        curDir.fetch();
+        var view = new DirectoryView({
+          model: curDir
+        });
+        $('#content').empty().append(view.el);
+      } else {
+        var view = new DirectoryView({
+          model: curDir
+        });
+        view.render();
+        $('#content').empty().append(view.el);
+      }
+    }
   },
 
   movies: function(path) {
