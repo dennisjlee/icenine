031c15e2ff0df07fd6ef50cce97baa9a749d983e
DJ Lee <dennisjlee@gmail.com>
2013-04-19 09:40:18 -0400

    Initial app skeleton generated by yeoman

---
+++ icenine2/app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('icenine2App')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
